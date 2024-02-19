5fd5d6839d4a2cc547ae69bd93f0c07712ed3d48
SoonHyunAn <ash7106@gmail.com>
2024-02-19 17:26:22 +0900

    Java_20240219

--- src/main/java/ch09_cookie_session/user/UserController.java
+++ src/main/java/ch09_cookie_session/user/UserController.java
@@ -45,25 +45,23 @@ public class UserController extends HttpServlet {
 				rd.forward(request, response);
 			} else {
 				uid = request.getParameter("uid");
+				User result = uSvc.getUserByUid(uid);
 				pwd = request.getParameter("pwd");
 				pwd2 = request.getParameter("pwd2");
-				User result = uSvc.getUserByUid(uid);
-				if (result == uSvc.correct_login) {
-					user = uSvc.getUserByUid(uid);
-					session.setAttribute("sessUid", uid);
-					session.setAttribute("sessUname", user.getUname());
-					msg = user.getUname() + "님 환영합니다.";
-					url = "/jw/ch09/user/list?page=1"; // 초기화면
-				} else if (result == uSvc.WRONG_PASSWORD) {
-					msg = "패스워드가 틀립니다.";
-					url = "/jw/ch09/user/login";
-				} else {
-					msg = "등록되지 않은 사용자입니다.";
-					url = "/jw/ch09/user/login";
+				uname = request.getParameter("uname");
+				email = request.getParameter("email");
+				result.setPwd(pwd); result.setEmail(email);
+				result.setUid(uid); result.setUname(uname);
+				if(!pwd.equals(pwd2)){
+					msg = "비밀번호가 일치하지 않습니다.";
+					url = "/ch09/user/register.jsp";
+					rd = request.getRequestDispatcher("/ch09/user/alertMsg.jsp");
+					request.setAttribute("msg", msg);
+					request.setAttribute("url", url);
+					rd.forward(request, response);
 				}
-				rd = request.getRequestDispatcher("/ch09/user/alertMsg.jsp");
-				request.setAttribute("msg", msg);
-				request.setAttribute("url", url);
+				rd = request.getRequestDispatcher("/ch09/user/list.jsp");
+				request.setAttribute("user", result);
 				rd.forward(request, response);
 			}
 			break;
